816ec12a02322f8306406902dae6d22eb542fc6f
Casper Beyer <caspervonb@qup.io>
2015-07-06 07:32:48 +0800

    Force CI

--- test.js
+++ test.js
@@ -4,3 +4,7 @@ var watcher = fs.watch(process.cwd());
 watcher.on('change', function(event, filename) {
   console.log(event, filename);
 });
+
+process.nextTick(function() {
+  console.log('watcher ok');
+});
