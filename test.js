26ccb3d1352bd4eb65e09c886d37fa79c4a76bd3
Casper Beyer <caspervonb@qup.io>
2015-07-06 17:34:57 +0800

    Grow data

--- test.js
+++ test.js
@@ -1,11 +1,12 @@
 var fs = require('fs');
 
+var data = '';
 var watcher = fs.watch(process.cwd());
 watcher.on('change', function(event, filename) {
   console.log(event, filename);
 
   setTimeout(function() {
-    fs.writeFileSync('data.txt', 'data');
+    fs.writeFileSync('data.txt', data += 'data\n');
   }, 1000);
 });
 
@@ -13,4 +14,4 @@ process.nextTick(function() {
   console.log('watcher ok');
 });
 
-fs.writeFileSync('data.txt', 'data');
+fs.writeFileSync('data.txt', data);
