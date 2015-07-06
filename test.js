c6b738c1124676b7fdfc88f48e4d7a8d300f16ae
Casper Beyer <caspervonb@qup.io>
2015-07-06 17:26:33 +0800

    Add file writes

--- test.js
+++ test.js
@@ -3,8 +3,14 @@ var fs = require('fs');
 var watcher = fs.watch(process.cwd());
 watcher.on('change', function(event, filename) {
   console.log(event, filename);
+
+  setTimeout(function() {
+    fs.writeFileSync('data.txt', 'data');
+  }, 1000);
 });
 
 process.nextTick(function() {
   console.log('watcher ok');
 });
+
+fs.writeFileSync('data.txt', 'data');
